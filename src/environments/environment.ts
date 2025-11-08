87d1806344cfb492a9d478ebb33adacdfb17b870
Mavi Mods <139610566+MaviMods@users.noreply.github.com>
2025-11-08 21:36:37 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,11 @@
 export const environment = {
   production: true,
-  firebaseConfig: {
-    apiKey: "AIzaSyAzhzjaAr_yQIIEkRM2HfUaj_9hHb83BMo",
-    authDomain: "nano-studios.firebaseapp.com",
-    projectId: "nano-studios",
-    storageBucket: "nano-studios.firebasestorage.app",
-    messagingSenderId: "549434076269",
-    appId: "1:549434076269:web:4ddfc29fec8021f502a364",
-    measurementId: "G-LKTBM9FL8S"
+  firebaseConfig = {
+    apiKey: "AIzaSyAe-IVcNabf0USb73HFbhotLV39TYw5mOg",
+    authDomain: "nano-studio-app.firebaseapp.com",
+    projectId: "nano-studio-app",
+    storageBucket: "nano-studio-app.firebasestorage.app",
+    messagingSenderId: "635377881205",
+    appId: "1:635377881205:web:290f73f705fb10ed30683c"
   }
 };
